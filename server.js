f0fa6537faf55490cf775d0c89a240749a50c6fc
pekka566 <pekka.suomala@gmail.com>
2018-02-12 16:41:19 +0200

    initial commit.

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, function() {
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+  app.post('/quotes', (req, res) => {
+    console.log(req.body);
+  });
+});
