4b8ee78110a84d3934f68cad890d0a7ba58283eb
pekka566 <pekka.suomala@gmail.com>
2018-02-13 12:50:13 +0200

    Another test

--- server.js
+++ server.js
@@ -4,12 +4,37 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function() {
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
+const MongoClient = require('mongodb').MongoClient;
 
-  app.post('/quotes', (req, res) => {
-    console.log(req.body);
+var db;
+
+MongoClient.connect(
+  'mongodb://improvuser:uusikattaus@ds229438.mlab.com:29438/improv_exercises?authSource=improv_exercises',
+  (err, client) => {
+    if (err) return console.log(err);
+    db = client.db('improv_exercises'); // whatever your database name is
+    app.listen(3000, () => {
+      console.log('listening on 3000');
+    });
+  }
+);
+
+app.get('/', (req, res) => {
+  db
+    .collection('quotes')
+    .find()
+    .toArray(function(err, results) {
+      console.log(results);
+      // send HTML file populated with quotes here
+    });
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err);
+
+    console.log('saved to database');
+    res.redirect('/');
   });
 });
